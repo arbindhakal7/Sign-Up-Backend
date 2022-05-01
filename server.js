6c25de97dc3fea6f572852bf6164e519e97e0eea
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-05-01 17:38:10 +0545

    :sparkles: serverjs completed

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const route = require("./routes/route");
+const cors = require("cors");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -9,6 +11,9 @@ mongoose.connect(process.env.DATABASE_ACCESS, () => {
   console.log("Database Connected successfully");
 });
 
+app.use(express.json());
+app.use(cors());
+app.use("/app", route);
 app.listen(5000, () => {
   console.log("Server is running...");
 });
