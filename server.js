355b40d03695bdc2e002701582d760a72eb4a96c
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-05-01 17:32:03 +0545

    :construction: serverjs being constructed

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_ACCESS, () => {
+  console.log("Database Connected successfully");
+});
 
 app.listen(5000, () => {
   console.log("Server is running...");
